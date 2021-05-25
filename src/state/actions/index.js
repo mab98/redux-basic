364ca73c14d763d77df6bdd7d31746e067f4b722
Muhammad Abubakar <mhmdabubakar98@gmail.com>
2021-05-26 03:22:12 +0500

    async using thunk

--- src/state/actions/index.js
+++ src/state/actions/index.js
@@ -1,12 +1,20 @@
 export const ADD = 'ADD';
 export const SUBTRACT = 'SUBTRACT';
 
-export const add = (data) => ({
-  type: ADD,
-  payload: data,
-});
+export const add = (data) => {
+  return (dispatch) => {
+    dispatch({
+      type: ADD,
+      payload: data,
+    });
+  };
+};
 
-export const subtract = (data) => ({
-  type: SUBTRACT,
-  payload: data,
-});
+export const subtract = (data) => {
+  return (dispatch) => {
+    dispatch({
+      type: SUBTRACT,
+      payload: data,
+    });
+  };
+};
